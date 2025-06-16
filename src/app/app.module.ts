08e84da083811148ff81973b950edd9fafa78b9c
tanimomor <omoraltanim@gmail.com>
2025-06-16 12:13:39 +0600

    Module updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
